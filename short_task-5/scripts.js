54a2822d14cd9a71dde4ca69c60512634b3310f7
David Boles <me@davidbol.es>
2018-04-16 00:27:17 -0400

    Done with short task 5.

--- short_task-5/scripts.js
+++ short_task-5/scripts.js
@@ -1,11 +1,41 @@
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+function googleLoaded() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var coords = position.coords;
+    displayLocation(coords.latitude, coords.longitude);
+    initMap(coords.latitude, coords.longitude)
+  }, function(error) {
+    displayError();
+  });
+}
+
+//Display the map
+function initMap(latitude, longitude) {
+  var coords = {lat: latitude, lng: longitude};
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    zoom: 16,
+    center: coords
   });
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: coords,
     map: map
   });
+  mapDiv.style.display = 'inherit';
+}
+
+//Text stuff
+function hideWorkingMessage() {
+  document.getElementById('working_message').style.display = 'none';
+}
+
+function displayLocation(latitude, longitude) {
+  hideWorkingMessage();
+  document.getElementById('latitude').innerText = latitude;
+  document.getElementById('longitude').innerText = longitude;
+  document.getElementById('location_info').style.display = 'initial';
+}
+
+function displayError() {
+  hideWorkingMessage();
+  document.getElementById('error_message').style.display = 'initial';
 }
