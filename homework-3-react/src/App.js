0c67dba19d2a2bd5e26785e9f3c20fd84ea8f32f
David Boles <me@davidbol.es>
2018-04-21 19:07:57 -0700

    Homework 3 complete?

--- homework-3-react/src/App.js
+++ homework-3-react/src/App.js
@@ -1,5 +1,17 @@
+import EmailIcon from '@material-ui/icons/Email';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import InfoIcon from '@material-ui/icons/InfoOutline';
+import PhoneIcon from '@material-ui/icons/Phone';
+import StoreIcon from '@material-ui/icons/Store';
+import WebAssetIcon from '@material-ui/icons/WebAsset';
+import { Button, FormControl, FormHelperText, TextField } from 'material-ui';
+import Avatar from 'material-ui/Avatar';
+import ExpansionPanel, { ExpansionPanelDetails, ExpansionPanelSummary } from 'material-ui/ExpansionPanel';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Typography from 'material-ui/Typography';
 import React, { Component } from 'react';
 import MainHeader from './MainHeader';
+import './styles.css';
 
 class App extends Component {
   render() {
@@ -7,10 +19,150 @@ class App extends Component {
       <React.Fragment>
         <MainHeader/>
 
-        {/*<main>
-          <div />
-          <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
-        </main>*/}
+        <main style={{padding: '32px', paddingTop: '80px'}}>
+          {this.renderPageContent()}
+        </main>
+      </React.Fragment>
+    );
+  }
+
+  renderPageContent() {
+    var hash = window.location.hash.substr(1).toLowerCase();
+    if(hash === 'home') return this.renderHome();
+    else if(hash === 'products') return this.renderProducts();
+    else if (hash === 'location') return this.renderLocation();
+    else if (hash === 'contact_us') return this.renderContactUs();
+    else return this.renderHome();
+  }
+
+  renderHome() {
+    return (
+      <Typography>Welcome to the home page! You can go to our other pages by opening the navigation drawer.</Typography>
+    );
+  }
+
+  renderProducts() {
+    return (
+        <ExpansionPanel style={{backgroundColor: '#3f51b5'}}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon style={{color: '#fff'}} />}>
+            <Typography style={{color: '#fff'}}>All our Toyz!</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails style={{display: 'block'}}>
+            
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<InfoIcon />}>
+                <Typography>Bike</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails style={{display: 'block', textAlign: 'center'}}>
+                <img src='/images/toy1.jpg' alt='mini bike' style={{width: '100%', maxWidth: '400px'}}/>
+                <br/>
+                <Typography>
+                  Get ready to put the pedal to the injection molded plastic!
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<InfoIcon />}>
+                <Typography>Rubber Duck</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails style={{display: 'block', textAlign: 'center'}}>
+                <img src='/images/toy2.jpg' alt='rubber ducky' style={{width: '100%', maxWidth: '400px'}}/>
+                <br/>
+                <Typography>
+                  Rubber ducky, you're the one! You make bath time so much fun!
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<InfoIcon />}>
+                <Typography>LEGO Millenium Falcon</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails style={{display: 'block', textAlign: 'center'}}>
+                <img src='/images/toy3.png' alt='millenium falcon' style={{width: '100%', maxWidth: '400px'}}/>
+                <br/>
+                <Typography>
+                  Vroom vrrrooooooom!!!!
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<InfoIcon />}>
+                <Typography>Snap Circuits</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails style={{display: 'block', textAlign: 'center'}}>
+                <img src='/images/toy4.jpg' alt='snap circuits' style={{width: '100%', maxWidth: '400px'}}/>
+                <br/>
+                <Typography>
+                  Is that even really a toy at this point?
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<InfoIcon />}>
+                <Typography>Blocks</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails style={{display: 'block', textAlign: 'center'}}>
+                <img src='/images/toy5.jpg' alt='blocks' style={{width: '100%', maxWidth: '400px'}}/>
+                <br/>
+                <Typography>
+                  Good ol' blocks. Can't go wrong with these!
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+    );
+  }
+
+  renderLocation() {
+    return (
+      <Typography>When we open our store, we will let you know our location here even with directions from your place to it!!!</Typography>
+    );
+  }
+
+  renderContactUs() {
+    return (
+      <React.Fragment>
+        <List>
+          <ListItem style={{paddingLeft: 0}}>
+            <Avatar>
+              <EmailIcon />
+            </Avatar>
+            <ListItemText primary="helpdesk@ABCToyz.com" style={{overflowWrap: 'break-word'}} />
+          </ListItem>
+          <ListItem style={{paddingLeft: 0}}>
+            <Avatar>
+              <PhoneIcon />
+            </Avatar>
+            <ListItemText primary="(123) 456 7890" />
+          </ListItem>
+          <ListItem style={{paddingLeft: 0}}>
+            <Avatar>
+              <StoreIcon />
+            </Avatar>
+            <ListItemText primary="123 Example Lane, A City, CA" />
+          </ListItem>
+          <ListItem style={{paddingLeft: 0}}>
+            <Avatar>
+              <WebAssetIcon />
+            </Avatar>
+            <ListItemText>
+              <a style={{color: 'inherit'}} href='https://www.facebook.com'>See us on Facebook!</a>
+            </ListItemText>
+          </ListItem>
+        </List>
+        <br/>
+        <FormControl style={{marginTop: 32, width: '100%'}}>
+          <FormHelperText>Let us know what you think!</FormHelperText>
+          <TextField label="Name" style={{marginTop: 8, maxWidth: '200px', minWidth: 0}}/>
+          <TextField label="Email" style={{marginTop: 16, maxWidth: '200px', minWidth: 0}}/>
+          <TextField multiline label="Comment" style={{marginTop: 16, width: '100%', maxWidth: '600px', minWidth: 0}}/>
+          <Button variant='raised' color="primary" style={{marginTop: 16, maxWidth: '100px', minWidth: 0}}>Submit</Button>
+        </FormControl>
       </React.Fragment>
     );
   }
--- homework-3-react/src/MainHeader.js
+++ homework-3-react/src/MainHeader.js
@@ -1,31 +1,66 @@
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import BasketIcon from '@material-ui/icons/ShoppingBasket';
+import LocationIcon from '@material-ui/icons/LocationOn';
+import ContactIcon from '@material-ui/icons/ImportContacts';
 import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import List from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
+import Tooltip from 'material-ui/Tooltip';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import SwipeableDrawer from 'material-ui/SwipeableDrawer';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import React, { Component } from 'react';
 
-const drawerWidth = 240;
-
 class MainHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      left: false
+    }
+  }
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <AppBar position='fixed' style={{width: 'calc(100% - ' + {drawerWidth} + 'px)'}}>
+        <AppBar position="fixed">
           <Toolbar>
-            <Typography variant="title" style={{flex: 1}} color='inherit'>
-              Homework 3
+            <Typography variant="title" color="inherit" style={{flexGrow: 1}}>
+              ABC Toyz
             </Typography>
+
+            <Tooltip id="tooltip-icon" title="Home">
+              <IconButton color='inherit' aria-label="Home" onClick={function(){window.location.href='#home';window.location.reload();}}>
+                <HomeIcon/>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip id="tooltip-icon" title="Products">
+              <IconButton color='inherit' aria-label="Products" onClick={function(){window.location.href='#products';window.location.reload();}}>
+                <BasketIcon/>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip id="tooltip-icon" title="Location">
+              <IconButton color='inherit' aria-label="Location" onClick={function(){window.location.href='#location';window.location.reload();}}>
+                <LocationIcon/>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip id="tooltip-icon" title="Contact Us">
+              <IconButton color='inherit' aria-label="Contact Us" onClick={function(){window.location.href='#contact_us';window.location.reload();}}>
+                <ContactIcon/>
+              </IconButton>
+            </Tooltip>
           </Toolbar>
         </AppBar>
-
-        <Drawer variant="permanent" PaperProps={{position: 'relative', width: drawerWidth}}>
-          <List>
-            <Typography variant="title" color='inherit'>
-              Homework 3
-            </Typography>
-          </List>
-        </Drawer>
+        
       </React.Fragment>
     );
   }
